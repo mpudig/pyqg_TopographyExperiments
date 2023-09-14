83ffa02bca526cdaa52e40279fa66b01d2d6000e
Matt Pudig <mp6191@log-2.hpc.nyu.edu>
2023-09-14 15:31:03 -0400

    Changes to default parameters for higher drag.

--- execute_model/layered_model/params_layered_model.py
+++ execute_model/layered_model/params_layered_model.py
@@ -5,19 +5,18 @@ import model_run_functions as functions
 
             ### Save path ###
 
-# expt_name format: nxVALUE_nzVALUE_unforced/forced_linear/expstrat_hrmsVALUE_KtopoVALUE_K0VALUE_m0VALUE
-
-expt_name = '/nx256_nz12_unforced_linearstrat_hrms150_Ktopo15_initialK24_initialm4'   # Modify for each experiments
-path = '/scratch/mp6191/pyqg_expts' + expt_name + '/output'
+expt_name = '/<expt_name>'                                   # Modify for each experiments
+path = '/scratch/mp6191/pyqg_expts' + expt_name + '/output'  # Path for saving model output
 
 
 
             ### Grid ###
 
-nx = 512         # Number of grid cells in x, y
-nz = 12          # Number of grid cells in z
-Ld = 50.e3       # First deformation radius
-L = 36 * Ld      # Length of square domain
+nx = 512                            # Number of grid cells in x, y
+nz = 12                             # Number of grid cells in z
+Ld = 15.e3                          # First deformation radius
+ld = 2 * np.pi * Ld                 # First deformation wavelength
+L = 10 * ld                         # Length of square domain
 
 Hmax = 4000.                        # Depth of model
 z = np.linspace(0, - Hmax, nz + 1)  # Cell edges 
@@ -28,10 +27,10 @@ zc = z[:-1] - H / 2                 # Cell centers
 
             ### Control parameters ###
     
-kappa_star = 0.3
-beta_star = 0.
+kappa_star = 0.8                    # kappa* = kappa * ld / U = 1/(2*pi) * kappa * Ld / U
+beta_star = 0.                      # beta* = beta * ld^2 / U = 1/(2*pi) * beta * Ld^2 / U
 
-U0 = 0.02
+U0 = 0.01
 
 
             ### Planetary parameters ###
@@ -70,15 +69,18 @@ rough_modes, rough_radii = functions.rough_bottom(f0, g, rho, H)
 flat_modes = flat_modes / np.sign(flat_modes[0, :])
 rough_modes = rough_modes / np.sign(rough_modes[0, :])
 
-U = U0 * (rough_modes[:, 1] - (rough_modes[-1, 1] / rough_modes[-1, 2]) * rough_modes[:, 2])
-V = 0 * (rough_modes[:, 1] - (rough_modes[-1, 1] / rough_modes[-1, 2]) * rough_modes[:, 2])
+U = (rough_modes[:, 1] - (rough_modes[-1, 1] / rough_modes[-1, 2]) * rough_modes[:, 2])
+U = U0 * U / ((U.max() - U.min()) / 2)
+
+V = (rough_modes[:, 1] - (rough_modes[-1, 1] / rough_modes[-1, 2]) * rough_modes[:, 2])
+V = 0 * V / ((V.max() - V.min()) / 2)
 
 
 
             ### Random topography ###
     
-K_topo = 15
-h_rms = 150.
+K_topo = 60
+h_rms = 0.
 htop = functions.monoscale_random(L, nx, K_topo, h_rms)
 
 
@@ -86,26 +88,25 @@ htop = functions.monoscale_random(L, nx, K_topo, h_rms)
             ### Time parameters and threading ###
 
 Ti = Ld / np.max(U)                  # estimate of the most unstable e-folding time scale, also nondimensionalizing factor for time [s]
-dt = Ti / 400.                       # time step [s]
-dt = np.ceil(dt / (60*60)) * 60*60   # time step rounded to nearest hour [s] 
+dt = 7200.                           # time step [s]
 tmax = 750 * Ti                      # simulation time [s]
 
 tsnapstart = 0.                      # start time for yielding model states [s]
-tsnapint = 60 * 60 * 24 * 5          # time interval at which to yield model states [s]
+tsnapint = 60 * 60 * 6               # time interval at which to yield model states [s]
 
 tavestart = 0.                       # start time to begin averaging [s]; begins halfway through the first snapshot
 taveint = dt                         # time interval over which to accumulate averages [s]
 
-tc_save = 100                               # the number of model states to store in memory to save as .nc files - should be guided by the dimensionality of the dataset         
+tc_save = 96                         # the number of model states to store in memory to save as .nc files - should be guided by the dimensionality of the dataset         
 tc_save = np.ceil(tsnapint / dt * tc_save)
 
-ntd = 8
+ntd = 16
 
 
             ### Initial condition ###
 
 # The initial PV field will have energy concentrated at horizontal wavenumber K_0 and solely within vertical mode m_0, and with total energy equal to E_tot
-K_0 = 9.
+K_0 = 15.
 m_0 = 3.
 q_flat_mode = flat_modes[:, int(m_0)]
 q_rough_mode = rough_modes[:, int(m_0)]
